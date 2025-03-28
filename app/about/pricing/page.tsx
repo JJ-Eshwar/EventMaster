07c04cb03493b4d0af38008b4cce5110c711c714
JJ-Eshwar <jjeshwar776@gmail.com>
2025-03-28 20:49:21 +0530

    added the pricing

---
+++ app/about/pricing/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PricingPage = () => {
+  return (
+    <div>
+      <h1>Pricing Page</h1>
+      <p>This is the pricing page content.</p>
+    </div>
+  );
+};
+
+export default PricingPage;
